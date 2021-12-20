e44004f0876d94f2522826c2309b50bba486838a
Mercer,Jeff <jeff_mercer@vanguard.com>
2021-12-20 15:38:26 +0000

    CNGAP-7425: Add behave wrapper

--- behave_asl/cli.py
+++ behave_asl/cli.py
@@ -1,4 +1,8 @@
+import sys
+
+from .wrapper import main as behave_main
 
 
 def main():
-    pass
+    """Delegate to the behave wrapper"""
+    sys.exit(behave_main())
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+setup(
+    author="Vanguard",
+    author_email="opensource@vanguard.com",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    description="",
+    install_requires=["behave>=1.0.25"],
+    name="behave-asl",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "behave-asl = behave_asl.cli:main",
+        ]
+    },
+    url=(
+        "https://github.com/vanguard/behave-asl"
+    ),
+    version="0.1.0",
+)
