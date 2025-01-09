bf715c517f1ac6539ae69cebfae1deec6538b449
Mercer,Jeff <jeff_mercer@vanguard.com>
2025-01-09 18:55:33 +0000

    WARCH-224: Fix some urls and drop Python 3.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import re
 from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
-github_docs_base_url = "https://github.com/vanguard/behave-asl/blob/master/"
+github_docs_base_url = "https://github.com/vanguard-oss/behave-asl/blob/master/"
 # Have Pypi use FQDN to Github, but everywhere else use relative links
 long_description = re.compile(r"\(([a-zA-Z0-9_\-/]*.md)\)").sub(f"({github_docs_base_url}\\1)", long_description)
 
@@ -18,7 +18,6 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: Apache Software License',
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
@@ -41,6 +40,6 @@ setup(
             "behave-asl = behaveasl.cli:main",
         ]
     },
-    url=("https://github.com/vanguard/behave-asl"),
+    url=("https://github.com/vanguard-oss/behave-asl"),
     version="1.2.0",
 )
